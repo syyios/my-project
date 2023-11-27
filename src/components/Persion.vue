865b0d3412ed12959fb7a2694c264974a908862e
syyios <1085756893@qq.com>
2023-11-27 23:47:42 +0800

    搭建UI基础框架

--- src/components/Persion.vue
+++ src/components/Persion.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>人员列表:</h1>
-    <h3 style="color:red">Count组件求和为：{{sum}}</h3>
+    <h3 style="color:red">Count组件求和为:{{sum}}</h3>
 		<h3>列表中第一个人的名字是：{{firstPersonName}}</h3>
 		<input type="text" placeholder="请输入名字" v-model="name">
 		<button @click="add">添加</button>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="todo-header">
+    <input type="text" placeholder="输入你的任务, 按回车确认!" v-model="title" @keyup.enter="add">
+  </div>
+</template>
+
+<script>
+import {nanoid} from 'nanoid'
+export default {
+    name: "TodoHeader",
+    data() {
+        return {
+            title: '',
+        }
+    },
+    methods: {
+        add() {
+            if(!this.title.trim()) {
+                alert('输入不能为空!')
+            }
+            const todoObj = {
+                id: nanoid(),
+                title: this.title,
+            }
+            this.$emit('addTodo', todoObj)
+            this.title = ''
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
